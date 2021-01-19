56655df75f854ebf4e765cbdd06ce36f62eb0614
Redwan Karim Sony <redwankarimsony1455@gmail.com>
2021-01-20 01:09:33 +0600

    skeleton working code added

---
+++ dataset/plant_doc.py
@@ -0,0 +1,55 @@
+import os
+from PIL import Image
+
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+class ImageFolder(Dataset):
+    def __init__(self, root_path, image_size=224, box_size=256, split = 'Train', **kwargs):
+        if box_size is None:
+            box_size = image_size
+
+        self.filepaths = []
+        self.label = []
+        if split == 'Train':
+            path = os.path.join(root_path, 'train')
+        else:
+            path = os.path.join(root_path, 'test')
+        classes = sorted((os.listdir(path)))
+
+        for i, c in enumerate(classes):
+            for filename in sorted(os.listdir(os.path.join(path, c))):
+                self.filepaths.append(os.path.join(path, c, filename))
+                self.label.append(i)
+        self.n_classes = max(self.label) + 1
+
+        norm_params = {'mean': [0.485, 0.456, 0.406],'std': [0.229, 0.224, 0.225]}
+        normalize = transforms.Normalize(**norm_params)
+
+        if kwargs.get('augment'):
+            self.transform = transforms.Compose([transforms.RandomResizedCrop(image_size),
+                                                 transforms.RandomHorizontalFlip(),
+                                                 transforms.ToTensor(),
+                                                 normalize])
+        else:
+            self.transform = transforms.Compose([transforms.Resize(box_size),
+                                                transforms.CenterCrop(image_size),
+                                                transforms.ToTensor(),
+                                                normalize])    
+
+        def convert_raw(x):
+            mean = torch.tensor(norm_params['mean']).view(3, 1, 1).type_as(x)
+            std = torch.tensor(norm_params['std']).view(3, 1, 1).type_as(x)
+            return x * std + mean
+        self.convert_raw = convert_raw    
+
+    
+    def __len__(self):
+        return len(self.filepaths)
+
+    def __getitem__(self, i):
+        img = Image.open(self.filepaths[i]).convert('RGB')
+        return self.transform(img), self.label[i]
+
+
---
+++ train.py
@@ -0,0 +1,131 @@
+import os, argparse, yaml, time, copy
+import albumentations
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+from torchvision import models
+
+
+from dataset.plant_doc import ImageFolder
+
+
+def train_model(model, dataloaders, dataset_sizes, criterion, optimizer, scheduler, device, num_epochs=25 ):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+def main(config):
+    print(config)
+    train_data = ImageFolder(config['dataset_path'], split = 'Train', augment = True)
+    print(f'{len(train_data)} labeled training images found!' )
+
+    test_data = ImageFolder(config['dataset_path'], split = 'Test', augment = True)
+    print(f'{len(test_data)} labeled testing images found!' )
+    dataset_sizes = {'train': len(train_data), 'val': len(test_data)}
+
+
+    train_dl = torch.utils.data.DataLoader(train_data, batch_size=config['batch_size'], shuffle=True, num_workers=4)
+    test_dl = torch.utils.data.DataLoader(test_data, batch_size=config['batch_size'], shuffle=True, num_workers=4)
+
+    dataloaders = {'train': train_dl, 'val': test_dl}
+
+    device = torch.device(config['device'] if torch.cuda.is_available() else 'cpu')
+
+
+    model_ft = models.resnet18(pretrained=True)
+    num_ftrs = model_ft.fc.in_features
+    # Here the size of each output sample is set to 2.
+    # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+    model_ft.fc = nn.Linear(num_ftrs, 28)
+
+    model_ft = model_ft.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    # Observe that all parameters are being optimized
+    optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+
+    # Decay LR by a factor of 0.1 every 7 epochs
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    
+
+    model_ft = train_model(model_ft, dataloaders, dataset_sizes, criterion, optimizer_ft, exp_lr_scheduler,
+                       device,  num_epochs=config['epochs'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config')
+    parser.add_argument('--name', default=None)
+    parser.add_argument('--tag', default=None)
+    parser.add_argument('--gpu', default='0')
+    args = parser.parse_args()
+
+    config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
+    main(config)
